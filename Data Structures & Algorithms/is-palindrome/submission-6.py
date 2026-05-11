195b81dc6999c4a662fa53a9f59355a41d36385e
sahareV9 <71066773+sahareV9@users.noreply.github.com>
2026-05-11 20:02:25 +0530

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        input_string = re.sub(r'[^a-zA-Z0-9\s]', '', s).lower().replace(" ","");
+        isPalandrome = True;
+        if len(input_string) == 1:
+            return True;
+        i = 0;
+        j = len(input_string) - 1;
+        while j > 0:
+            if input_string[i] != input_string[j]:
+                isPalandrome = False;
+            j -= 1;
+            i += 1;
+            
+        return isPalandrome;
